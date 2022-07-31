f0fa7719675eb874a5755fc9f7d8c1a28b2a2498
Oleksandr Haidysh <OleksHaid@gmail.com>
2022-07-31 11:55:06 +0300

    fix prom3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,6 +7,7 @@ let step = null;
 let delay = null;
 let amount = null;
 let ct = null;
+let delay2 = null;
 
 function onSubmit(e) {
   e.preventDefault();
@@ -14,9 +15,13 @@ function onSubmit(e) {
   delay = Number(form[0].value);
   step = Number(form[1].value);
   amount = Number(form[2].value);
-  ct = 1
+  ct = 1;
+  delay2 = delay
 
-  setTimeout(() => createPromise(), delay);
+  for (let i = 1; i <= amount; i++) {
+    setTimeout(() => createPromise(), delay);
+    delay += step;
+  }
   form.reset();
 }
 
@@ -25,27 +30,22 @@ function createPromise() {
     const shouldResolve = Math.random() > 0.3;
 
     if (shouldResolve) {
-      resolve(`✅ Fulfilled promise ${ct} in ${delay}ms`);
+      resolve(`✅ Fulfilled promise ${ct} in ${delay2}ms`);
     }
-    reject(`❌ Rejected promise ${ct} in ${delay}ms`);
+    reject(`❌ Rejected promise ${ct} in ${delay2}ms`);
   });
 
   myPromise
     .then(res => {
       console.log(res);
       Notify.success('Sol lucet omnibus');
+      delay2 += step;
+      ct += 1;
     })
-    .then(callAgain())
     .catch(error => {
       console.log(error);
       Notify.failure('Please, choose date in the future');
+      delay2 += step;
+      ct += 1;
     });
-}
-
-function callAgain() {
-  delay += step;
-  ct += 1;
-  if (ct <= amount) {
-    setTimeout(() => createPromise(), delay);
-  }
 }
